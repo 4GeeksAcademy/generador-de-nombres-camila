d837ebe122ca0a7ee540616402919164e4d3375e
felixcamila <138735721+felixcamila@users.noreply.github.com>
2024-07-01 20:03:22 +0000

    agregar for en app.js

--- src/app.js
+++ src/app.js
@@ -7,5 +7,43 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon", "internet", "ratio"];
+  let extension = [".com", ".net", ".us", ".io"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < extension.length; l++) {
+          if (
+            noun[k].substring(noun[k].length - 3) ==
+            extension[l].substring(extension[l].length - 3)
+          ) {
+            console.log(
+              pronoun[i] +
+                adj[j] +
+                noun[k].substring(0, noun[k].length - 3) +
+                extension[l]
+            );
+          } else if (
+            noun[k].substring(noun[k].length - 2) ==
+            extension[l].substring(extension[l].length - 2)
+          ) {
+            console.log(
+              pronoun[i] +
+                adj[j] +
+                noun[k].substring(0, noun[k].length - 2) +
+                extension[l]
+            );
+          } else {
+            console.log(pronoun[i] + adj[j] + noun[k] + extension[l]);
+          }
+        }
+      }
+    }
+  }
+
   console.log("Hello Rigo from the console!");
 };
